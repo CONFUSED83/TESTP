2882adb7e095c7b687b671fb47c05516c5923097
confused <113919939+CONFUSED83@users.noreply.github.com>
2026-04-01 11:22:37 -0700

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,6 +1,2 @@
 // OneSignal Service Worker
-try {
-    self.importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
-} catch (e) {
-    console.log('OneSignal worker load warning:', e);
-}
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
